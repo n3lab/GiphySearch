00fa2702d9a20ecc9e0b22f6c4b4dbe3019d58dc
n3lab <n3laboratory@yandex.ru>
2020-07-06 12:03:51 +0300

    Initial Commit

---
+++ GiphySearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GiphySearch
+//
+//  Created by n3deep on 06.07.2020.
+//  Copyright © 2020 n3deep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
